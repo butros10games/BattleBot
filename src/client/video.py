d5c9d6b78b596a7cfe003f394ca7dd40045323ca
Butros Groot MacBook <butrosgroot@gmail.com>
2024-03-05 12:10:44 +0100

    Commit Type: Test
    [Short one-line description of the change]
    
    Signed-off-by: ButrosGroot <butrosgroot@gmail.com>

--- src/client/video.py
+++ src/client/video.py
@@ -9,38 +9,15 @@ class VideoWindow:
         self.window_name = window_name
         self.win_error = False
         self.depth_map = False
+        self.cap = cv2.VideoCapture(0)
         # cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
 
     def display_frame(self, frame):
-        # Convert the av.VideoFrame to a numpy array in RGB format
-        img_bgr = frame.to_ndarray(format="rgb24")
-
-        # Correctly convert RGB image to BGR for display with OpenCV and invert the colors
-        #img_bgr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR) # Convert the image to BGR
-        img_bgr_flipped = cv2.flip(img_bgr, -1)
-
-        scale_percent = 50 # percent of original size
-        width = int(img_bgr.shape[1] * scale_percent / 100)
-        height = int(img_bgr.shape[0] * scale_percent / 100)
-        dim = (width, height)
-        # resize image
-        img_bgr = cv2.resize(img_bgr_flipped,  dim, interpolation = cv2.INTER_AREA)
+        ret, frame_e = self.cap.read()
+        if not ret:
+            print("Failed to grab a frame")
         
-        cv2.imshow(self.window_name, img_bgr)
-        
-        # Get the screen size
-        screen_width, screen_height = self._get_system_metrics()
-
-        # Calculate the position to center the window
-        x = (screen_width // 2) - (width // 2)
-        y = (screen_height // 2) - (height // 2)
-
-        # Move the window to the center of the screen
-        cv2.moveWindow(self.window_name, x, y)
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            return True  # Indicate that the window should close
-        return False  # Indicate that the window should not close
+        cv2.imshow('frame', frame_e)
     
     def _get_system_metrics(self):
         """
